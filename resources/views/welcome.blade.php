b846360346411bb22bc3906c6ba6bf735a9b6b22
CesarAlejandro-ux <160355721+CesarAlejandro-ux@users.noreply.github.com>
2024-03-22 15:14:17 -0600

    cambios de titulo en el archivo welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel Modificado con repositorio Git</title>
+        <title>Laravel Modificado con repositorio Git clonacion de proyecto</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
